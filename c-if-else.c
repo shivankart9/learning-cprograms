f26668c3ea4f04155e65d95752be698e4e562e36
shivankart9 <shivankart9@gmail.com>
2026-02-28 15:35:43 +0530

    eligible or not

---
+++ c-if-else.c
@@ -0,0 +1,17 @@
+/*this code is of if else in which if you are eligible or not*/
+#include <stdio.h>
+int main()
+{
+    int age;
+    printf("enter your age:");
+    scanf("%d", &age);
+    if(age>=18)
+    {
+        printf("you are eligible");
+    }
+    else
+    {
+        printf("you are not eligible");
+    }
+    return 0;
+}
