7cee2efce4a2af1b8a6b8a8d5c5473ab9f19f176
shivankart9 <shivankart9@gmail.com>
2026-03-08 21:25:08 +0530

    program for sum of two numbers

---
+++ num-sum.c
@@ -0,0 +1,14 @@
+// this program gives sum of two number
+#include <stdio.h>
+
+int main() {
+    float a,b,c;
+    printf("Enter The Number 1: ");
+    scanf("%f",&a);
+    printf("Enter The Number 2: ");
+    scanf("%f",&b);
+    c=a+b;
+    printf("the sum is %f",c);
+    
+    return 0;
+}
