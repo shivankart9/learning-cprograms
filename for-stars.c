2325267c90714f1b1d70f2fff3d466e080b52a7f
shivankart9 <shivankart9@gmail.com>
2026-03-18 20:17:16 +0530

    fix: code got deleted so i added again

---
+++ for-stars.c
@@ -0,0 +1,15 @@
+// this program prints the stars using for loop
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=3;i++)
+    {
+    for(j=1;j<=i;j++)
+    {
+    printf("*");
+    }
+    printf("\n");
+    }
+    return 0;
+}
